885523bca1f8ad13d632054c3737a7682e55fedc
Matt Clement <darkmoves@gmail.com>
2013-12-13 23:00:22 -0500

    Change Station model and filename to the singular.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Station.create(name:'Alewife' , lat: 42.395704, long: -71.141099)
+Station.create(name:'Davis' , lat: 42.396197, long: -71.121926)
+Station.create(name:'Porter' , lat: 42.388505, long: -71.119148)
+Station.create(name:'Harvard' , lat: 42.373653, long: -71.118954)
+Station.create(name:'Central' , lat: 42.365457, long: -71.103644)
+Station.create(name:'Kendall/MIT' , lat: 42.362413, long: -71.086382)
+Station.create(name:'Charles/MGH' , lat: 42.361255, long: -71.070825)
+Station.create(name:'Park St' , lat: 42.356332, long: -71.061952)
+Station.create(name:'Downtown Crossing' , lat: 42.355515, long: -71.060493)
+Station.create(name:'South Station' , lat: 42.351971, long: -71.056008)
+Station.create(name:'Broadway' , lat: 42.342884, long: -71.057349)
+Station.create(name:'Andrew' , lat: 42.329362, long: -71.056845)
+Station.create(name:'JFK/UMASS' , lat: 42.320732, long: -71.051717)
+Station.create(name:'Savin Hill' , lat: 42.312672, long: -71.052747)
+
+
+
