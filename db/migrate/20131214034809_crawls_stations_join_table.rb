24e28618bd44b7f4eb1fb9bf71aa60f430e5f453
Matt Clement <darkmoves@gmail.com>
2013-12-16 14:48:19 -0500

    Deleted Crawl, Station, Bar and Crawl Station Join Table migrations.

--- db/migrate/20131214034809_crawls_stations_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CrawlsStationsJoinTable < ActiveRecord::Migration
-  def change
-  	create_table :crawls_stations, id: false do |t|
-  		t.integer :crawl_id
-  		t.integer :station_id
-  	end
-  end
-end
