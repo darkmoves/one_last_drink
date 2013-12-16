c7d1b10b15155714ec3a752895d902fde685e7a5
Matt Clement <darkmoves@gmail.com>
2013-12-16 17:58:24 -0500

    Added final train times and line to seeds. Minor changes to BarsStations join table.

--- db/migrate/20131216214514_create_bars_stations_join_table.rb
+++ db/migrate/20131216214514_create_bars_stations_join_table.rb
@@ -1,6 +1,6 @@
 class CreateBarsStationsJoinTable < ActiveRecord::Migration
   def change
-       create_table :bars_stations do |t|
+    create_table :bars_stations do |t|
       t.belongs_to :bar
       t.belongs_to :station
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-alewife = Station.create(name:'Alewife' , lat: 42.395588, long: -71.141948)
-davis = Station.create(name:'Davis' , lat: 42.396197, long: -71.121926)
-porter = Station.create(name:'Porter' , lat: 42.388505, long: -71.119148)
-harvard = Station.create(name:'Harvard' , lat: 42.373653, long: -71.118954)
-central = Station.create(name:'Central' , lat: 42.365603, long: -71.104031)
-kendall_mit = Station.create(name:'Kendall/MIT' , lat: 42.362413, long: -71.086382)
-charles_mgh = Station.create(name:'Charles/MGH' , lat: 42.361255, long: -71.070825)
-park_st = Station.create(name:'Park St' , lat: 42.356332, long: -71.061952)
-downtown_crossing = Station.create(name:'Downtown Crossing' , lat: 42.355515, long: -71.060493)
-south_station = Station.create(name:'South Station' , lat: 42.351971, long: -71.056008)
-broadway = Station.create(name:'Broadway' , lat: 42.342884, long: -71.057349)
-andrew = Station.create(name:'Andrew' , lat: 42.329362, long: -71.056845)
-jfk_umass = Station.create(name:'JFK/UMASS' , lat: 42.320732, long: -71.051717)
-savin_hill = Station.create(name:'Savin Hill' , lat: 42.312672, long: -71.052747)
+Station.create(name:'Alewife', lat: 42.395588, long: -71.141948, last_train_nb: nil,last_train_sb: "0:22", line: 'red')
+Station.create(name:'Davis', lat: 42.396197, long: -71.121926, last_train_nb: "1:15",last_train_sb: "0:25", line: 'red')
+Station.create(name:'Porter', lat: 42.388505, long: -71.119148, last_train_nb: "1:12",last_train_sb: "0:28", line: 'red')
+Station.create(name:'Harvard', lat: 42.373653, long: -71.118954, last_train_nb: "1:09",last_train_sb: "0:31", line: 'red')
+Station.create(name:'Central', lat: 42.365603, long: -71.104031, last_train_nb: "1:06",last_train_sb: "0:34", line: 'red')
+Station.create(name:'Kendall/MIT', lat: 42.362413, long: -71.086382, last_train_nb: "1:03",last_train_sb: "0:37", line: 'red')
+Station.create(name:'Charles/MGH', lat: 42.361255, long: -71.070825, last_train_nb: "1:00",last_train_sb: "0:40", line: 'red')
+Station.create(name:'Park St', lat: 42.356332, long: -71.061952, last_train_nb: "0:57",last_train_sb: "0:43", line: 'red')
+Station.create(name:'Downtown Crossing', lat: 42.355515, long: -71.060493, last_train_nb: "0:54", last_train_sb: "0:46", line: 'red')
+Station.create(name:'South Station', lat: 42.351971, long: -71.056008, last_train_nb: "0:51",last_train_sb: "0:49", line: 'red')
+Station.create(name:'Broadway', lat: 42.342884, long: -71.057349, last_train_nb: "0:48",last_train_sb: "0:52", line: 'red')
+Station.create(name:'Andrew', lat: 42.329362, long: -71.056845, last_train_nb: "0:45",last_train_sb: "0:55", line: 'red')
+Station.create(name:'JFK/UMASS', lat: 42.320732, long: -71.051717, last_train_nb: "0:42",last_train_sb: "0:58", line: 'red')
+Station.create(name:'Savin Hill', lat: 42.312672, long: -71.052747, last_train_nb: "0:39",last_train_sb: "1:01", line: 'red')
