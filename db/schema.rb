4e60fce4b6b276c6a5069cb8d1e2e078e47997cf
Matt Clement <darkmoves@gmail.com>
2013-12-16 16:52:37 -0500

    Drop, create DB and migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131216214514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bars", force: true do |t|
+    t.text     "name"
+    t.text     "address"
+    t.decimal  "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bars_stations", force: true do |t|
+    t.integer "bar_id"
+    t.integer "station_id"
+  end
+
+  create_table "stations", force: true do |t|
+    t.text     "name"
+    t.decimal  "lat"
+    t.decimal  "long"
+    t.time     "last_train_nb"
+    t.time     "last_train_sb"
+    t.text     "line"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
