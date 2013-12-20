f9de4401349fb4fa1336eabf7020e80e2596f64e
Matt Clement <darkmoves@gmail.com>
2013-12-19 20:29:41 -0500

    Add js to toggle visibility of other line station forms whenever one is selected.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,27 +6,21 @@ var Page = {
 					})
 				},
 
-				// slideToViz: function(line_id, form_id){
-				// 	$(line_id).click(function(){
-				// 		if ($(form_id).is(':hidden')){
-				// 			$(form_id).slideDown('slow');
-				// 		} else {
-				// 			$(form_id).hide();
-				// 		}
-				// 	})
-				// }
-
-				slideToViz: function(line_id, form_id){
+				slideToViz: function(line_id, form_id, form_id_2, form_id_3, form_id_4){
 					$(line_id).click(function(){
 						$(form_id).slideToggle('slow');
+						$(form_id_2).slideUp('slow');
+						$(form_id_3).slideUp('slow');
+						$(form_id_4).slideUp('slow');
 					})
 				}
 
 };
 
 $(function(){
-				Page.slideToViz('#red-line-container', '#red-line-form');
-				Page.slideToViz('#orange-line-container', '#orange-line-form');
-				Page.slideToViz('#green-line-container', '#green-line-form');
-				Page.slideToViz('#blue-line-container', '#blue-line-form');
+				// Page.slideToViz('#red-line-container', '#red-line-form');
+				Page.slideToViz('#red-line-container', '#red-line-form', '#orange-line-form', '#green-line-form', '#blue-line-form');
+				Page.slideToViz('#orange-line-container', '#orange-line-form', '#red-line-form', '#green-line-form', '#blue-line-form');
+				Page.slideToViz('#green-line-container', '#green-line-form', '#red-line-form', '#orange-line-form', '#blue-line-form');
+				Page.slideToViz('#blue-line-container', '#blue-line-form', '#red-line-form', '#orange-line-form', '#green-line-form');
 })
