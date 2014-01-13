1738098fd4f2ca05a36a8180cde10e500ed62823
Matt Clement <darkmoves@gmail.com>
2014-01-13 17:20:19 -0500

    Commit after re-installing rails in the project dir.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,31 +8,35 @@ class MainController < ApplicationController
 	end
 
 	def show
-		# Define variables via params hash
-		@station = Station.find(params[:id])
-		direction = params[:direction]
 
-		# Select a bar hash from the Yelp API using the find_bar class method.
-		@bar = @station.find_bar
+			# Define variables via params hash
+			@station = Station.find(params[:id])
+			direction = params[:direction]
 
-		# Extract bar attributes from the bar hash to display in the view.
-		@name = @bar['name']
-		@address = @bar['location']['display_address'].split.join(", ")
-		@rating = @bar['rating']
+		# if @station.name = 'Alewife' && direction = 'nb'	
+			
 
-		# Select the last train time using the last_train method and direction variable, taken from the params hash.
-		# Declare the current time in UTC to match the time zone of the scheduled last trains. 		
-		t_train = @station.last_train(direction)
-		t_now = Time.now.utc
+			# Select a bar hash from the Yelp API using the find_bar class method.
+			@bar = @station.find_bar
 
-		# Get the time remaining until the last train using the time_comparison class method. Returns a 2 element hash with hours, minutes.
-		time = Calculation.time_comparison(t_train, t_now)
+			# Extract bar attributes from the bar hash to display in the view.
+			@name = @bar['name']
+			@address = @bar['location']['display_address'].split.join(", ")
+			@rating = @bar['rating']
 
-		# Create the variable to display time using the elements from the time hash. format_time  method adds a 0 before single digit minutes for display.
-		@time = Calculation.format_time(time)
-		
-		# Use the time difference to pick a message to display.
-		@message = Calculation.find_message(@time, @name)
+			# Select the last train time using the last_train method and direction variable, taken from the params hash.
+			# Declare the current time in UTC to match the time zone of the scheduled last trains. 		
+			t_train = @station.last_train(direction)
+			t_now = Time.now.utc
+
+			# Get the time remaining until the last train using the time_comparison class method. Returns a 2 element hash with hours, minutes.
+			time = Calculation.time_comparison(t_train, t_now)
+
+			# Create the variable to display time using the elements from the time hash. format_time  method adds a 0 before single digit minutes for display.
+			@time = Calculation.format_time(time)
+			
+			# Use the time difference to pick a message to display.
+			@message = Calculation.find_message(@time, @name)
 
 	end
 
