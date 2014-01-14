e430a64bddc6f5e9e52f46802c0c23af40d82fe6
Matt Clement <darkmoves@gmail.com>
2014-01-13 23:33:01 -0500

    Add logic to handle instance when selected station and destination are the same.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -20,19 +20,32 @@ class MainController < ApplicationController
 		@address = @bar['location']['display_address'].split.join(", ")
 		@rating = @bar['rating']
 
-		# Select the last train time using the last_train method and direction variable, taken from the params hash.
-		# Declare the current time in UTC to match the time zone of the scheduled last trains. 		
-		t_train = @station.last_train(direction)
-		t_now = Time.now.utc
+		if (@station.name == 'Alewife' || @station.name == 'Oak Grove' || @station.name == 'Lechmere' || @station.name == 'Wonderland') && (direction == 'nb')
 
-		# Get the time remaining until the last train using the time_comparison class method. Returns a 2 element hash with hours, minutes.
-		time = Calculation.time_comparison(t_train, t_now)
+			@valid = false
 
-		# Create the variable to display time using the elements from the time hash. format_time  method adds a 0 before single digit minutes for display.
-		@time = Calculation.format_time(time)
-		
-		# Use the time difference to pick a message to display.
-		@message = Calculation.find_message(@time, @name)
+		elsif (@station.name == 'Ashmont' || @station.name == 'Braintree' || @station.name == 'Forest Hills' || @station.name == 'Boston College' || @station.name == 'Cleveland Circle' || @station.name == 'Riverside' || @station.name == 'Heath Street' || @station.name == 'Bowdoin') && (direction == 'sb')
+
+			@valid = false
+
+		else
+			
+			@valid = true
+			# Select the last train time using the last_train method and direction variable, taken from the params hash.
+			# Declare the current time in UTC to match the time zone of the scheduled last trains. 		
+			t_train = @station.last_train(direction)
+			t_now = Time.now.utc
+
+			# Get the time remaining until the last train using the time_comparison class method. Returns a 2 element hash with hours, minutes.
+			time = Calculation.time_comparison(t_train, t_now)
+
+			# Create the variable to display time using the elements from the time hash. format_time  method adds a 0 before single digit minutes for display.
+			@time = Calculation.format_time(time)
+			
+			# Use the time difference to pick a message to display.
+			@message = Calculation.find_message(@time, @name)
+
+		end
 
 	end
 
