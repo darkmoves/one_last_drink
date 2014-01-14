1eb35136db0d30db564b4a1fbcfd9a5bf7bd7769
Matt Clement <darkmoves@gmail.com>
2014-01-13 23:17:03 -0500

    Commit.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -12,7 +12,6 @@ class MainController < ApplicationController
 		@station = Station.find(params[:id])
 		direction = params[:direction]
 
-
 		# Select a bar hash from the Yelp API using the find_bar class method.
 		@bar = @station.find_bar
 
@@ -21,26 +20,19 @@ class MainController < ApplicationController
 		@address = @bar['location']['display_address'].split.join(", ")
 		@rating = @bar['rating']
 
-		# Check validity of station and direction
-		if (@station.name == 'Alewife') && (direction == 'nb')
-			@valid = false
-		else
-			@valid = true
+		# Select the last train time using the last_train method and direction variable, taken from the params hash.
+		# Declare the current time in UTC to match the time zone of the scheduled last trains. 		
+		t_train = @station.last_train(direction)
+		t_now = Time.now.utc
 
-			# Select the last train time using the last_train method and direction variable, taken from the params hash.
-			# Declare the current time in UTC to match the time zone of the scheduled last trains. 		
-			t_train = @station.last_train(direction)
-			t_now = Time.now.utc
+		# Get the time remaining until the last train using the time_comparison class method. Returns a 2 element hash with hours, minutes.
+		time = Calculation.time_comparison(t_train, t_now)
 
-			# Get the time remaining until the last train using the time_comparison class method. Returns a 2 element hash with hours, minutes.
-			time = Calculation.time_comparison(t_train, t_now)
-
-			# Create the variable to display time using the elements from the time hash. format_time  method adds a 0 before single digit minutes for display.
-			@time = Calculation.format_time(time)
-			
-			# Use the time difference to pick a message to display.
-			@message = Calculation.find_message(@time, @name)
-		end
+		# Create the variable to display time using the elements from the time hash. format_time  method adds a 0 before single digit minutes for display.
+		@time = Calculation.format_time(time)
+		
+		# Use the time difference to pick a message to display.
+		@message = Calculation.find_message(@time, @name)
 
 	end
 
