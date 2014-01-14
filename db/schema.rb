ef2df1ccabb2602d6513d59e245159059071869e
Matt Clement <darkmoves@gmail.com>
2014-01-14 11:29:26 -0500

    Perform migration. Edit red line seeds to include new endpoint boolean data.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216214422) do
+ActiveRecord::Schema.define(version: 20140114162005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20131216214422) do
     t.text     "line"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "nb_endpoint"
+    t.boolean  "sb_endpoint"
   end
 
 end
