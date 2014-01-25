d719602b5509b722b946a54b04dacc434cc7859f
Matt Clement <darkmoves@gmail.com>
2014-01-25 15:33:20 -0500

    Add main controller rspec tests for rendering index view and assigning stations to the instance variables for the 4 T lines.

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -9,6 +9,21 @@ describe MainController do
 			expect(response).to be_success
 			expect(response.status).to eq(200)
 		end
+
+		it "renders the index template" do
+			get :index
+			expect(response).to render_template("index")
+		end
+
+		it "properly assigns T stations to each of the 4 lines" do
+			station1, station2, station3, station4 = Station.create(line: 'red'), Station.create(line: 'orange'),Station.create(line: 'green'),Station.create(line: 'blue')
+
+			get :index
+			expect(assigns(:red_line_stations)).to eq([station1])
+			expect(assigns(:orange_line_stations)).to eq([station2])
+			expect(assigns(:green_line_stations)).to eq([station3])
+			expect(assigns(:blue_line_stations)).to eq([station4])
+		end
 	end
 
 end
